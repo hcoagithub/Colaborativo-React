accb0b5054e9167ea231dda26f530dd8ace890dd
hcoagithub <hectorcoa.14@gmail.com>
2021-10-15 01:13:59 +0000

    Esta es la base del Code React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Card from './Card';
+import Jumbotron from './Jumbotron';
+
+function App (){
+    return (<>
+    <Navbar/>
+    <Card/>
+    <Jumbotron/>
+    </>);
+}
+
+export default App;
---
+++ src/Jumbotron.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Jumbotron  (){
+    return (<h1>Jumbotron</h1>);
+}
+
+export default Jumbotron;
