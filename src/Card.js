34e8f6d0f745a3ef103d916e3e9f0c3b09dfd93e
plopez1985 <pl586546@gmail.com>
2021-10-15 03:48:54 +0000

    Cards

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,31 @@
 import React from 'react';
-
+import PropTypes from "prop-types";
 function Card (){
-    return (<h1> Card </h1>);
+    const cardstyle = {
+        "width": "100%",
+    }
+    return (
+    <div className="col-lg-3 col-md-6 col-sm-12">
+        <div className="card" style={cardstyle} >
+            <img src={props.imgenUrl} className="card-img-top" alt={props.imgenAlt} />
+            <div className="card-body">
+                <h5 className="card-title">{props.title}</h5>
+                <p className="card-text">{props.cardBody}</p>
+                <a href={props.btnUrl} className="btn btn-primary">{props.btnLabel}</a>
+            </div>
+        </div>
+    </div>
+);
 }
+Card.propTypes = {
+    imgenUrl: PropTypes.string,
+    imgenAlt: PropTypes.string,
+    title: PropTypes.string,
+    cadBody: PropTypes.string,
+    btnUrl: PropTypes.string,
+    btnLabel: PropTypes.string,
 
-export default Card;
+
+};
+
+export default Card; 
---
+++ src/data.js
@@ -0,0 +1,38 @@
+export let data = [
+    {
+      imgenUrl: "https://picsum.photos/id/237/400/300",
+      imgenAlt: "perro",
+      title: "Cachorrito",
+      cardBody:
+        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      btnUrl: "#",
+      btnLabel: "Go somewhere",
+    },
+    {
+      imgenUrl: "https://picsum.photos/id/236/400/300",
+      imgenAlt: "casa",
+      title: "Casa de playa",
+      cardBody:
+        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      btnUrl: "#",
+      btnLabel: "Go somewhere",
+    },
+    {
+      imgenUrl: "https://picsum.photos/id/238/400/300",
+      imgenAlt: "ciudad",
+      title: "Metropolis",
+      cardBody:
+        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      btnUrl: "#",
+      btnLabel: "Go somewhere",
+    },
+    {
+      imgenUrl: "https://picsum.photos/id/235/400/300",
+      imgenAlt: "paisaje",
+      title: "Naturaleza",
+      cardBody:
+        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      btnUrl: "#",
+      btnLabel: "Go somewhere",
+    },
+  ];
